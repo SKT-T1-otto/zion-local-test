3936cbf5980ed9cad44026c165ef0ec1826bbf91
SKT-T1-otto <sktotto970@gmail.com>
2022-07-18 16:20:07 +0800

    部署了加密货币源码，https://learnblockchain.cn/docs/solidity/introduction-to-smart-contracts.html

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
 )
 
 func main() {
-	ethClient, err := ethclient.Dial("http://localhost:22000")
+	client, err := ethclient.Dial("http://localhost:22000")
+	//address_erc20 := "0x9b2233dcfa0f3bf82ec734af762477eb2eda7b12"
 	if err != nil {
 		log.Fatal("client err", err)
 	}
-
+	checkTx(client, common.HexToHash("0x460ad592631cb7f3a65976732fdfbddb1f65c27c9eb3c743348b49da6b872638"))
+	//deployErc20Contract(client)
 }
-
--- zion-local-simpleContract.go
+++ zion-local-simpleContract.go
@@ -102,9 +102,19 @@ func sendTx(client *ethclient.Client) {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	value := big.NewInt(9000000000000000000) // in wei (1 eth)
-	gasLimit := uint64(21000)                // in units
+	//var Tx = require('ethereumjs-tx').Transaction;
+	//var privateKey = Buffer.from('e331b6d69882b4cb4ea581d88e0b604039a3de5967688d3dcffdd2270c0fd109', 'hex');
+	//
+	//var rawTx = {
+	//nonce: '0x00',
+	//	gasPrice: '0x09184e72a000',
+	//		gasLimit: '0x2710',
+	//		to: '0x0000000000000000000000000000000000000000',
+	//		value: '0x00',
+	//		data: '0x7f74657374320000000000000000000
+	value := big.NewInt(0x00)
+	// in wei (1 eth)
+	gasLimit := uint64(21000) // in units
 	gasPrice, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
 		log.Fatal(err)
